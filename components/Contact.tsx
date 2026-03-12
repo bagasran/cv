bd1c08a6c7d925fd496f8491c00e65d666843d6d
Bagas Randika <bagas.gamesmax@gmail.com>
2026-03-12 14:06:47 +0700

    update cv

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,11 +2,20 @@ export default function Contact() {
   return (
     <section id="contact" className="py-20 bg-orange-100 text-center">
       <h2 className="text-3xl font-bold mb-6">Contact</h2>
-
-      <p className="text-gray-700">Email : bagas.gamesmax@gmail.com</p>
-
-      <p className="text-gray-700">Phone : +62 82246924080</p>
-
+      <a
+        href="mailto:bagas.gamesmax@gmail.com"
+        className="text-blue-500 hover:underline"
+      >
+        bagas.gamesmax@gmail.com
+      </a>
+      <hr />
+      <a
+        href="https://wa.me/6282246924080"
+        target="_blank"
+        className="text-green-600 underline"
+      >
+        Phone : +62 82246924080
+      </a>
       <p className="text-gray-700">Location : Kota Cimahi</p>
     </section>
   );
